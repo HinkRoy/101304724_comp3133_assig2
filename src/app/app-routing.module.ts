39e697d4a8e09d270f8c3117b8627483e6c609b5
HinkRoy <zh101304724@gmail.com>
2024-04-07 01:19:27 +0800

    finish login feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {SignUpComponent} from "./sign-up/sign-up.component";
+import {EmployeeListComponent} from "./employee-list/employee-list.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginComponent,
+    component: EmployeeListComponent,
   },
   {
     path: 'login',
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import {Apollo, gql} from "apollo-angular";
+import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
+
+const LOGIN = gql`
+  query login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      id
+      username,
+      email
+    }
+  }
+`;
 
 @Component({
   selector: 'app-login',
@@ -6,5 +19,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = ""
+  password: string = ""
 
+  constructor(
+    private apollo: Apollo,
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ) {
+  }
+
+  login() {
+    console.log(this.username, this.password)
+    if (this.username && this.password) {
+      const user = this.apollo.mutate({
+        mutation: LOGIN,
+        variables: {
+          username: this.username,
+          password: this.password,
+        }
+      }).subscribe((res: any) => {
+        if (res.data.login) {
+          this._snackBar.open("Login successful!");
+          localStorage.setItem("user", JSON.stringify(res.data.login));
+          this.router.navigateByUrl('/login')
+        } else {
+          this._snackBar.open("Invalid username or password!");
+        }
+      }, error => {
+        this._snackBar.open("Invalid username or password!");
+      })
+    }
+  }
 }
